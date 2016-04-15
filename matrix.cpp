f4bb2e1ac5a663e649bba3906fcc7df8884559fb
Ethan <ethanabrooks@gmail.com>
2016-04-15 00:45:08 -0400

    almost working

--- matrix.cpp
+++ matrix.cpp
@@ -8,7 +8,11 @@
 using namespace std;
 using namespace arma;
 
-mutex mtx;
+struct SingularMatrixException : public exception {
+  const char* what () const throw () {
+    return "Cannot invert a singular matrix";
+  }
+};
 
 bool withinLight(glm::vec2 objPos,
                  QList<Light*> lights) {
@@ -41,11 +45,6 @@ mat getCoords(QVector<QVector<int> >* board,
                     }
                 }
             }
-            if ((*board)[i][j] == 2) { // frog
-                for (auto k: {i, j}) {
-                    coords.push_back((float) k);
-                }
-            }
         }
     }
     return mat (&coords[0], // pointer to data
@@ -56,7 +55,7 @@ mat getCoords(QVector<QVector<int> >* board,
 
 mat getCentroids(mat coords, int num) {
     mat centroids;
-    bool success = kmeans(centroids, coords, num,
+    kmeans(centroids, coords, num,
            random_spread, // initializer
            10, // num rounds
            false); // print progress?
@@ -70,35 +69,36 @@ vec getLineParams(vec p1, vec p2) {
   mat x = line1.col(0);
   mat y = line1.col(1);
   mat augX = join_horiz(x, ones(x.size()));
+  if (det(augX) == 0) {
+    throw SingularMatrixException();
+  }
   return solve(augX, y);
-}
-
-mat getIntercept(vec startpoint1, vec endpoint1,
-                vec startpoint2, vec endpoint2) {
-  vec lineParams1 = getLineParams(startpoint1, endpoint1);
-  vec lineParams2 = getLineParams(startpoint2, endpoint2);
-  mat params = join_horiz(lineParams1, lineParams2).t();
-  vec xParams = params.col(0);
-  mat augXParams = join_horiz(-xParams, ones(xParams.size()));
-  return solve(augXParams, params.col(1));
 }
 
 bool liesBetween(vec linePoint1, vec linePoint2, vec point) {
   double x1 = linePoint1[0];
   double x2 = linePoint2[0];
   double xPoint = point[0];
-  return (x1 < xPoint && xPoint < x2) || (x1 > xPoint && xPoint > x2);
+  double e = .01;
+  if (x1 == x2) {
+    x1 = linePoint1[1];
+    x2 = linePoint2[1];
+    xPoint = point[1];
+  }
+  cout << " x1 " << x1 << endl;
+  cout << " x2 "  << x2 << endl;
+  cout << " xPoint " << xPoint << endl;
+  cout << " x1 < xPoint " << (x1 < xPoint) << endl;
+  return (x1 + e < xPoint && xPoint + e < x2)
+      || (x1 > xPoint + e && xPoint > x2 + e);
 }
 
 vec getVerticalIntercept(double x, mat params) {
     return vec({x, dot(params, vec({x, 1}))});
 }
 
-bool intersects(vec startpoint1, vec endpoint1, vec startpoint2, vec endpoint2) {
-  vec lineParams1 = getLineParams(startpoint1, endpoint1);
-  vec lineParams2 = getLineParams(startpoint2, endpoint2);
-
-  vec intercept;
+mat getIntercept(vec startpoint1, vec endpoint1, 
+                vec startpoint2, vec endpoint2) {
 
   double x1 = startpoint1[0];
   double x2 = startpoint2[0];
@@ -106,24 +106,37 @@ bool intersects(vec startpoint1, vec endpoint1, vec startpoint2, vec endpoint2) 
   bool line1isVertical = x1 == endpoint1[0];
   bool line2isVertical = x2 == endpoint2[0];
 
-  if (line1isVertical && line2isVertical) {
-    return false; // the lines are parallel; they don't intersect;
-  }
   if (line1isVertical) {
-    intercept = getVerticalIntercept(x1, lineParams2);
+    return getVerticalIntercept(x1, getLineParams(startpoint2, endpoint2));
   } else if (line2isVertical) {
-    intercept = getVerticalIntercept(x2, lineParams1);
+    return getVerticalIntercept(x2, getLineParams(startpoint1, endpoint1));
   } else {
-    mat params = join_horiz(lineParams1, lineParams2).t();
+    mat params = join_horiz( 
+        getLineParams(startpoint1, endpoint1), 
+        getLineParams(startpoint2, endpoint2)
+        ).t();
     vec xParams = params.col(0);
     mat augXParams = join_horiz(-xParams, ones(xParams.size()));
-    if (det(augXParams) == 0) { // this means that the lines are parallel
-      return false;
+    if (det(augXParams) == 0) {
+      throw SingularMatrixException();
     }
-    intercept = solve(augXParams, params.col(1));
+    return solve(augXParams, params.col(1));
   }
-  return liesBetween(startpoint1, endpoint1, intercept)
-      && liesBetween(startpoint2, endpoint2, intercept);
+}
+
+bool intersects(vec startpoint1, vec endpoint1, 
+                vec startpoint2, vec endpoint2) {
+  //vec lineParams1 = getLineParams(startpoint1, endpoint1);
+  //vec lineParams2 = getLineParams(startpoint2, endpoint2);
+  vec intercept;
+  try {
+    intercept = getIntercept(startpoint1, endpoint1, startpoint2, endpoint2);
+  } catch (SingularMatrixException) {
+    return false;
+  }
+  bool l1 = liesBetween(startpoint1, endpoint1, intercept);
+  bool l2 = liesBetween(startpoint2, endpoint2, intercept);
+  return l1 && l2;
 }
 
 bool intersects(vec startpoint, vec endpoint, wall_nodes wall) {
@@ -135,6 +148,14 @@ Node graphBetween(Node here, Node there, vector<wall_nodes> walls) {
   bool straightShot = true;
   for (wall_nodes wall : walls) {
     if (intersects(here.coordinate, there.coordinate, wall)) {
+        cout << "here " << here.coordinate[0] << endl;
+        cout << "here " << here.coordinate[1] << endl;
+        cout << "there " << there.coordinate[0] << endl;
+        cout << "there " << there.coordinate[1] << endl;
+        cout << "wall1 " << wall.point1.coordinate[0] << endl;
+        cout << "wall1 " << wall.point1.coordinate[1] << endl;
+        cout << "wall2 " << wall.point2.coordinate[0] << endl;
+        cout << "wall2 " << wall.point2.coordinate[1] << endl;
       straightShot = false;
       Node wall1 = graphBetween(wall.point1, there, walls);
       Node wall2 =graphBetween(wall.point2, there, walls);
@@ -147,24 +168,31 @@ Node graphBetween(Node here, Node there, vector<wall_nodes> walls) {
   }
 }
 
+vec extend(vec near, vec far, double by) {
+  vec offset = normalise(near - far) * by;
+  return near + offset;
+}
+
 Node getWallNode(vec near, vec far, double lightRadius) {
-  vec offset = normalise(near - far);
-  vec position = near + offset;
+  vec position = extend(near, far, lightRadius);
   vector<double> bounds = {0, 500};
-  vector<vec> corners;
-  for (double xCoord: bounds) {
-      for (double yCoord: bounds) {
-          corners.push_back(vec({xCoord, yCoord}));
-      }
+  vector<pair<vec, vec>> edges;
+  for (float k = 0; k < bounds.size(); k+=.5) {
+      int a = k;
+      int b = int (k + .5) % bounds.size();
+      int c = int (k + 1) % bounds.size();
+      vec start({bounds[a], bounds[b]});
+      vec end({bounds[b], bounds[c]});
+      edges.push_back(pair<vec, vec>(
+            extend(start, end, 1),
+            extend(end, start, 1)
+            ));
   }
-  vector<Pair<vec, vec>> edges;
-  for (int i = 0; i < corners.size() + 1; i++) {
-      int j = (i + 1) % corners.size();
-      edges.push_back(Pair<corners[i], corners[j]>);
-  }
-  for (Pair<vec, vec> edge :  edges) {
-      if (intersects(near, offset, edge.first, edge.second)) {
-          position = getIntercept(near, offset, edge.first, edge.second);
+  for (pair<vec, vec> edge :  edges) {
+//    cout << "first" << endl << edge.first <<
+//      endl << "second" << endl << edge.second << endl;
+      if (intersects(far, position, edge.first, edge.second)) {
+          position = getIntercept(far, position, edge.first, edge.second);
       }
   }
   return Node(position);
